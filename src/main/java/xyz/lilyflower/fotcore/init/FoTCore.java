cc78f75e0b6bc2fde2f171ca87d57a00a0e8389d
Lily Autumn <realprismaticyt@gmail.com>
2025-02-13 14:55:49 +1300

    forgor about these two

--- src/main/java/xyz/lilyflower/fotcore/init/FoTCore.java
+++ src/main/java/xyz/lilyflower/fotcore/init/FoTCore.java
@@ -6,11 +6,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import xyz.lilyflower.fotcore.init.config.ConfigLoader;
 
-@Mod(modid = "gtm", version = FoTCore.VERSION, dependencies = "")
+@Mod(modid = "fotcore", version = FoTCore.VERSION, dependencies = "")
 public class FoTCore {
     public static final String VERSION = "2.1";
 
-    public static final Logger LOGGER = LogManager.getLogger("GenericTweakMod");
+    public static final Logger LOGGER = LogManager.getLogger("FoT Core");
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
